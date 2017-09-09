dd835a8e5528129ae961f5b2688db161153a20bd
James Xue <james.xue@columbia.edu>
2017-09-09 02:08:03 -0400

    Get MusixMatch lyrics for hardcoded song

--- content.js
+++ content.js
@@ -1,4 +1,24 @@
-// This executes when the page is loaded
-console.log('Page loaded');
+// This executes when the extension icon is clicked
+console.log('Icon clicked');
 
+document.addEventListener('DOMContentLoaded', function () {
+	const lyrics = generateLyrics("One Dance Drake");
+	document.getElementById("div-lyrics").innerHTML = lyrics;
+});
 
+// API call to MusixMatch 
+function generateLyrics(songTitle) {
+	console.log(songTitle);
+	const theUrl = `https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?q_track=${songTitle}&apikey=8155f760c67aff660b8e04d67fd000ac`
+	
+	const xmlHttp = new XMLHttpRequest();
+	xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+	xmlHttp.send( null );
+	const response = JSON.parse(xmlHttp.responseText);
+	
+	console.log(response.message);
+	if (!response.message.body.lyrics.lyrics_body) {
+		return "Cannot find song lyrics"
+	}
+	return response.message.body.lyrics.lyrics_body;
+}
